24d879d8c95624ef0dc7be961f3f5ac650c0f902
Jason Gowan <gowan.jason@gmail.com>
2017-06-21 05:20:28 -0700

    add ChromeService to start and stop the browser

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 'use strict';
 
-module.exports.Driver = require('./lib/core').Driver;
+const Driver = require('./lib/core').Driver;
+const ChromeService = require('./lib/chrome_service').ChromeService;
+
+
+module.exports = function(options, callback) {
+
+    if (typeof options === 'function') {
+        callback = options;
+        options = undefined;
+    }
+    var service = new ChromeService(options);
+
+    service.start().then((driver) => {
+        return callback(driver);
+    }).then(() => {
+        service.stop();
+    }).catch((err) => {
+        console.error(err.stack || err);
+
+        service.stop();
+    });
+};
+
+module.exports.Driver = Driver;
+module.exports.ChromeService = ChromeService;
--- test/core.js
+++ test/core.js
@@ -2,6 +2,7 @@
 
 const assert = require('assert');
 const Driver = require('./../index').Driver;
+const ChromeService = require('./../index').ChromeService;
 const CDP = require('chrome-remote-interface');
 const fs = require('fs');
 const expect = require('chai').expect
@@ -11,185 +12,139 @@ describe('core api', function() {
     const baseHtml = fs.readFileSync(__dirname + '/../fixtures/static/base.html', 'utf-8');
     const baseUrl = 'http://127.0.0.1:8080';
     var mockServer;
+    var driver;
+    var service;
 
     before(function(done) {
         mockServer = createMockServer(8080, function() {
             console.log('start mock server');
-            done();
+
+            service = new ChromeService();
+            service.start().then((result) => {
+                driver = result;
+                done();
+            });
         });
     });
 
     after(function() {
+        console.log('stop mock server');
         mockServer.close();
+        service.stop();
     });
 
     describe('query', function() {
         it('can query elements', function(done) {
-            CDP((client) => {
-                var driver;
-                Driver.createDriver(client).then((result) => {
-                    driver = result;
-                    return driver.setContent(baseHtml);
-                }).then(() => {
-                    return driver.querySelectorAll({selector: 'div'})
-                }).then((nodes) => {
-                    expect(nodes.length).to.be.above(1);
+            driver.setContent(baseHtml).then(() => {
+                return driver.querySelectorAll({selector: 'div'})
+            }).then((nodes) => {
+                expect(nodes.length).to.be.above(1);
 
-                    client.close();
-                    done();
-                }).catch((err) => {
+                done();
+            }).catch((err) => {
 
-                    client.close();
-                    done(err);
-                });
+                done(err);
             });
         });
 
         it('can query element', function(done) {
-            CDP((client) => {
-                var driver;
-                var outerNode;
-                Driver.createDriver(client).then((result) => {
-                    driver = result;
-                    return driver.setContent(baseHtml);
-                }).then(() => {
-                    return driver.querySelector({selector: 'div#outer'})
-                }).then((node) => {
-                    outerNode = node;
-                    return outerNode.getAttribute('class');
-                }).then((value) => {
-                    expect(value).to.equal('outer');
-                    return outerNode.getAttributes();
-                }).then((attrs) => {
-                    expect(attrs).to.deep.equal({id: 'outer', class: 'outer'});
+            var outerNode;
+            driver.setContent(baseHtml).then(() => {
+                return driver.querySelector({selector: 'div#outer'})
+            }).then((node) => {
+                outerNode = node;
+                return outerNode.getAttribute('class');
+            }).then((value) => {
+                expect(value).to.equal('outer');
+                return outerNode.getAttributes();
+            }).then((attrs) => {
+                expect(attrs).to.deep.equal({id: 'outer', class: 'outer'});
 
-                    client.close();
-                    done();
-                }).catch((err) => {
+                done();
+            }).catch((err) => {
 
-                    client.close();
-                    done(err);
-                });
+                done(err);
             });
         });
 
         it('can query element from node', function(done) {
-            CDP((client) => {
-                var driver;
-                Driver.createDriver(client).then((result) => {
-                    driver = result;
-                    return driver.setContent(baseHtml);
-                }).then(() => {
-                    return driver.querySelector({selector: 'div#outer'})
-                }).then((node) => {
-                    return node.querySelector({selector: 'div#inner'});
-                }).then((node) => {
-                    return node.getAttribute('class');
-                }).then((value) => {
-                    expect(value).to.equal('inner');
+            driver.setContent(baseHtml).then(() => {
+                return driver.querySelector({selector: 'div#outer'})
+            }).then((node) => {
+                return node.querySelector({selector: 'div#inner'});
+            }).then((node) => {
+                return node.getAttribute('class');
+            }).then((value) => {
+                expect(value).to.equal('inner');
 
-                    client.close();
-                    done();
-                }).catch((err) => {
+                done();
+            }).catch((err) => {
 
-                    client.close();
-                    done(err);
-                });
+                done(err);
             });
         });
+    });
 
-        it('can query elements from node', function(done) {
-            CDP((client) => {
-                var driver;
-                Driver.createDriver(client).then((result) => {
-                    driver = result;
-                    return driver.setContent(baseHtml);
-                }).then(() => {
-                    return driver.querySelector({selector: 'div#outer'})
-                }).then((node) => {
-                    return node.querySelectorAll({selector: 'div'});
-                }).then((nodes) => {
-                    expect(nodes.length).to.be.above(1);
+    it('can query elements from node', function(done) {
+        driver.setContent(baseHtml).then(() => {
+            return driver.querySelector({selector: 'div#outer'})
+        }).then((node) => {
+            return node.querySelectorAll({selector: 'div'});
+        }).then((nodes) => {
+            expect(nodes.length).to.be.above(1);
 
-                    client.close();
-                    done();
-                }).catch((err) => {
+            done();
+        }).catch((err) => {
 
-                    client.close();
-                    done(err);
-                });
-            });
+            done(err);
         });
 
         it('can navigate to google.com', function(done) {
-            CDP((client) => {
-                var driver;
-                Driver.createDriver(client).then((result) => {
-                    driver = result;
-                    return driver.navigate({url: 'https://www.google.com'});
-                }).then(() => {
-                    return driver.querySelector({selector: 'input[name="q"]'})
-                }).then(() => {
-                    client.close();
-                    done();
-                }).catch((err) => {
+            driver.navigate({url: 'https://www.google.com'}).then(() => {
+                return driver.querySelector({selector: 'input[name="q"]'})
+            }).then(() => {
+                client.close();
+                done();
+            }).catch((err) => {
 
-                    client.close();
-                    done(err);
-                });
+                client.close();
+                done(err);
             });
         });
+    });
 
-        it('can click an element', function(done) {
-            CDP((client) => {
-                var driver;
-                Driver.createDriver(client).then((result) => {
-                    driver = result;
-                    return driver.navigate({url: baseUrl + '/clickable.html'});
-                }).then(() => {
-                    return driver.querySelector({selector: 'div#inner'})
-                }).then((node) => {
-                    return node.click();
-                }).then(() => {
-                    return driver.querySelector({selector: 'div#innerClick'})
-                }).then(() => {
-                    client.close();
-                    done();
-                }).catch((err) => {
+    it('can click an element', function(done) {
+        driver.navigate({url: baseUrl + '/clickable.html'}).then(() => {
+            return driver.querySelector({selector: 'div#inner'})
+        }).then((node) => {
+            return node.click();
+        }).then(() => {
+            return driver.querySelector({selector: 'div#innerClick'})
+        }).then(() => {
+            done();
+        }).catch((err) => {
 
-                    client.close();
-                    done(err);
-                });
-            });
+            done(err);
         });
+    });
 
-        it('can send keys to an element', function(done) {
-            var inputElem;
-            var name = 'naru';
-            CDP((client) => {
-                var driver;
-                Driver.createDriver(client).then((result) => {
-                    driver = result;
-                    return driver.navigate({url: baseUrl + '/input.html'});
-                }).then(() => {
-                    return driver.querySelector({selector: 'input#name'})
-                }).then((node) => {
-                    inputElem = node;
-                    return inputElem.sendKeys(name);
-                }).then((node) => {
-                    return inputElem.getProperty('value');
-                }).then((value) => {
-                    expect(value).to.equal(name);
-                }).then(() => {
-                    client.close();
-                    done();
-                }).catch((err) => {
+    it('can send keys to an element', function(done) {
+        var inputElem;
+        var name = 'naru';
+        driver.navigate({url: baseUrl + '/input.html'}).then(() => {
+            return driver.querySelector({selector: 'input#name'})
+        }).then((node) => {
+            inputElem = node;
+            return inputElem.sendKeys(name);
+        }).then((node) => {
+            return inputElem.getProperty('value');
+        }).then((value) => {
+            expect(value).to.equal(name);
+        }).then(() => {
+            done();
+        }).catch((err) => {
 
-                    client.close();
-                    done(err);
-                });
-            });
+            done(err);
         });
-
     });
 });
