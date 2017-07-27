aece2185c2d6ac7ff30528d5b85f2f4ac14c9b15
Jason Gowan <gowan.jason@gmail.com>
2017-07-27 04:21:55 -0700

    support none type page load strategy
    the 'none' strategy does not wait for the page to load.

--- fixtures/server.js
+++ fixtures/server.js
@@ -2,9 +2,15 @@
 
 var http = require('http')
 var Router = require('node-simple-router')
+var url = require('url')
 
 module.exports = function (port, callback) {
   var router = new Router({static_route: __dirname + '/static'})
+  router.get('/sleep', function(req, resp) {
+      const ms = parseInt(url.parse(req.url, true).query.time) * 1000
+      resp.setHeader('Content-Type', 'text/html; charset=UTF-8')
+      setTimeout(function() { resp.end('<p>Hello</p>') }, ms)
+  })
   var server = http.createServer(router)
   server.listen(port, callback)
   return server
---
+++ test/page_load.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const assert = require('assert')
+const Driver = require('./../index').Driver
+const ExpectedConditions = require('./../index').ExpectedConditions
+const ChromeService = require('./../index').ChromeService
+const CDP = require('chrome-remote-interface')
+const fs = require('fs')
+const expect = require('chai').expect
+const createMockServer = require('./../fixtures/server')
+
+describe('page load', function () {
+  const baseHtml = fs.readFileSync(__dirname + '/../fixtures/static/base.html', 'utf-8')
+  const baseUrl = process.env.CHROMINATOR_MOCK_SERVER_BASE_URL
+  var driver
+  var service
+
+  before(function (done) {
+    service = new ChromeService()
+    service.start().then((result) => {
+      driver = result
+      done()
+    })
+  })
+
+  after(function (done) {
+    service.stop().then(() => {
+        done()
+    })
+  })
+
+  it('undefined strategy should not wait for page load', function(done) {
+      driver.navigate({url: baseUrl + '/sleep?time=5', pageLoadStrategy: 'none'}).then(() => {
+        done()
+      }).catch((err) => {
+        done(err)
+      })
+  }).timeout(1000)
+
+})
